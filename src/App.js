3d0c7370fb4b179278dfb79453c02d066ef94403
Hopp Murithi <hoppmurithi@gmail.com>
2022-10-26 19:18:59 +0300

    Add meme input components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header"
+import Meme from "./components/form"
 
 export default function App() {
   return (
    <div>
       <Header />
+      <Meme />
    </div>
   );
 }
