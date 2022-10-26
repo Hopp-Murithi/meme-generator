7cf19420d64b204b7dd50f022ccc0343705cc734
Hopp Murithi <hoppmurithi@gmail.com>
2022-10-26 18:56:04 +0300

    Create header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+  return (
+    <div className="header">
+    <img src="https://img.freepik.com/premium-vector/fish-angler-logo-design-template_730679-4.jpg?size=626&ext=jpg" alt="mock"/>
+      <h1 className="header-title">header components</h1>
+      <h4 className="header-project">React project</h4>
+    </div>
+  )
+}
