83b3ceb4242d55b37911d87c9bd7ea0a52439835
Hopp Murithi <hoppmurithi@gmail.com>
2022-10-31 22:53:41 +0300

    Add random meme images

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,29 @@
+ // eslint-disable-next-line
 import React from "react";
+import { useState } from "react";
 import memeData from "../memeData";
 
 
-let url
+
 export default function Meme() {
+  //const [memeImage, setMemeImage] = React.useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImg: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages,setAllMemeImages] = useState(memeData);
+
+
+
   function getMemeImage() {
-    const meme = memeData.data.memes;
+    const meme = allMemeImages.data.memes;
     const randomIndex = Math.floor(Math.random() * meme.length);
-    url = meme[randomIndex].image;
-    alert(url);
+    const url = meme[randomIndex].image;
+    setMeme(prevImg => ({
+      ...prevImg,
+      randomImg: url,
+    }));
   }
   
   return (
@@ -30,6 +45,8 @@ export default function Meme() {
     />
       <button className="meme-button" onClick={getMemeImage}>Get new meme image</button>
     </div>
+      <img className="meme-image" src={meme.randomImg} alt="meme" />
+    
     </main>
   );
 }
